812f1db9ee57b5afb08095e2204fe6460a499525
gtopolyai <gerely01@gmail.com>
2015-11-22 13:58:01 +0100

    gitignire, google play version upgrade, map fragment improve

--- aVest/src/main/java/com/topolyai/avest/android/VestSupportMapFragment.java
+++ aVest/src/main/java/com/topolyai/avest/android/VestSupportMapFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.topolyai.avest.Bootstrap;
 
@@ -17,10 +18,35 @@ public class VestSupportMapFragment extends SupportMapFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         bootstrap = DefaultBootstrap.get();
         bootstrap.registerObject(this, true);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        mapInited(getMap());
+        return view;
     }
 
+    protected void mapInited(GoogleMap map) {
+
+    }
     public Bootstrap getBootstrap() {
         return bootstrap;
     }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        if (hidden) {
+            hide();
+        } else {
+            show();
+        }
+        super.onHiddenChanged(hidden);
+    }
+
+
+    protected void hide() {
+
+    }
+
+    protected void show() {
+
+    }
+
 }
